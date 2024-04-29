9ab32c779364affb9cd40c25c81cbc4b325189d2
21xiaoye <lao73957459414@163.com>
2024-04-29 18:43:13 +0800

    fix(Ter-common): 修改了open Api
    添加了许可证

--- Ter-tools/Ter-common/src/main/java/com/cabin/ter/config/SwaggerConfig.java
+++ Ter-tools/Ter-common/src/main/java/com/cabin/ter/config/SwaggerConfig.java
@@ -1,10 +1,24 @@
 package com.cabin.ter.config;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.Schema;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *     Swagger 生成API文档
+ * </p>
+ *
+ * @author xiaoye
+ * @date Created in 2024-04-29 18:23
+ */
 
 @Configuration
 public class SwaggerConfig {
@@ -23,10 +37,15 @@ public class SwaggerConfig {
         contact.setName("邺子意");
         contact.setUrl("https://github.com/21xiaoye");
 
+
+        License license = new License();
+        license.setName("GNU General Public License v3.0");
+        license.setUrl("https://github.com/21xiaoye/Ter/blob/main/LICENSE");
         return new Info()
                 .title("Ter-接口文档")
                 .description("Ter 项目文档")
                 .version("v1")
-                .contact(contact);
+                .contact(contact)
+                .license(license);
     }
 }
