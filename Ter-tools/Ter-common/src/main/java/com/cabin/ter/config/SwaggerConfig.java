f56d6f8801a91b3a9b01a76d33a00c3b1952b90e
21xiaoye <lao73957459414@163.com>
2024-04-29 12:07:11 +0800

    feat(Ter-common): 自动生成Api文档功能
    使用swagger添加自动生成Api文档功能

---
+++ Ter-tools/Ter-common/src/main/java/com/cabin/ter/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.cabin.ter.config;
+
+import io.swagger.annotations.ApiImplicitParams;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo()).select()
+//                .apis(RequestHandlerSelectors.basePackage("com.cabin.ter.controller"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(ApiImplicitParams.class))
+                .paths(PathSelectors.any()).build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Ter")
+                .description("Ter Api测试文档")
+                .contact(new Contact("xiaoye", null, "zouye0113@gmail.com")).version("1.0.0-SNAPSHOT").build();
+    }
+}
